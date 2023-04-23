f2edc104f009f71e73a1600575ce2991e8139186
SolarDub <81772405+SolarDub@users.noreply.github.com>
2023-04-23 14:56:08 -0400

    compact c_v_l

--- src/f90/calc_vel_components.f90
+++ src/f90/calc_vel_components.f90
@@ -5,10 +5,10 @@ SUBROUTINE calc_velocity_components(r, s, t, u, v, w)
 
   IMPLICIT NONE
 
-  INTEGER :: i, j, jn, js
+  INTEGER :: i, j, jn, js, isum
   INTEGER :: l, l1, l2, m, m1, m2
   INTEGER :: fac, ieo
-  REAL(kind=SP) :: theta, x, sintheta, rst, em, hfac, v1, v2
+  REAL(kind=SP) :: theta, x, sintheta, rst, rstterm, em, hfac, v1, v2
 
 ! Spherical Harmonic coefficients: coef; Associated Legendre Polynomials, p
   REAL(kind=SP)       :: coef(nx,nx), p(nx)
@@ -18,23 +18,17 @@ SUBROUTINE calc_velocity_components(r, s, t, u, v, w)
 
 ! Sums of northern (odd) and southern (even) hemispheric spectra coefficients
 ! for given order, m, over all degrees, l, at given latitude acos(x)
-  COMPLEX(kind=SP)    :: sum1, sum2, sum3, sum4, sum5, sum6
+! 1, 2: Toroidal N/S; 4, 4: Poloidal N/S; 5, 6: Radial N/S;
+  COMPLEX(kind=SP), DIMENSION(6) :: sum
 
 ! Northern and southern hemispheric velocity vector components:
-! phi, u (toroidal); theta, v (poloidal); radius, w (radial)
-
-  COMPLEX(kind=SP), DIMENSION(nphi) :: unorth, usouth
-  COMPLEX(kind=SP), DIMENSION(nphi) :: vnorth, vsouth
-  COMPLEX(kind=SP), DIMENSION(nphi) :: wnorth, wsouth
+! phi: 1, 2 (toroidal); theta: 3, 4 (poloidal); radius:5, 6 (radial)
+! Odd: North; Even: South
+  COMPLEX(kind=SP), DIMENSION(6,nphi) :: vc = (0., 0.)
 
 ! Velocity vector component maps
 ! Increase dimension by 4 to produce wrap-around border for interpolation
   REAL(kind=SP), DIMENSION(nphi+4,nx+4) :: u, v, w
-
-! Initialize complex spectralcoefficients in both hemispheres to zero
-  unorth = (0., 0.); usouth = (0., 0.)
-  vnorth = (0., 0.); vsouth = (0., 0.)
-  wnorth = (0., 0.); wsouth = (0., 0.)
 
 !  Calculate Associated Legendre Polynomial coefficients
 	CALL plmcoef(coef)
@@ -74,13 +68,8 @@ SUBROUTINE calc_velocity_components(r, s, t, u, v, w)
 ! Produce assoc. Legendre polynomials for current latitude
       CALL plm(m,x,coef,p)
 
-! Reset complex velocity spectral amplitude arrays
-      sum1 = (0.,0.)  ! Toroidal, North
-      sum2 = (0.,0.)  ! Toroidal, South
-      sum3 = (0.,0.)  ! Poloidal, North
-      sum4 = (0.,0.)  ! Poloidal, South
-      sum5 = (0.,0.)  ! Radial, North
-      sum6 = (0.,0.)  ! Radial, South
+! Reset cumulative complex velocity spectral amplitude array
+      sum = (0.,0.)
 
       fac  = CEILING(em/(em+1))    ! m = 0, fac = 0; fac = 1 otherwise
       hfac = 1. - 0.5*fac          ! m = 0, hfac = 1.0; hfac = 0.5 otherwise
@@ -95,35 +84,31 @@ SUBROUTINE calc_velocity_components(r, s, t, u, v, w)
         v2   = -m*p(l1)*fac
 
 ! Sum toroidal, poloidal, radial velocity amplitudes
-        sum1 = sum1 - t(l1,m1)*v1 + xi*s(l1,m1)*v2
-        sum2 = sum2 + ieo * (t(l1,m1)*v1 + xi*s(l1,m1)*v2)
-        sum3 = sum3 + s(l1,m1)*v1 + xi*t(l1,m1)*v2
-        sum4 = sum4 - ieo * (s(l1,m1)*v1 - xi*t(l1,m1)*v2)
-        sum5 = sum5 + r(l1,m1)*p(l1)
-        sum6 = sum6 + ieo * r(l1,m1)*p(l1)
+        sum(1) = sum(1) - t(l1,m1)*v1 + xi*s(l1,m1)*v2
+        sum(2) = sum(2) + ieo * (t(l1,m1)*v1 + xi*s(l1,m1)*v2)
+        sum(3) = sum(3) + s(l1,m1)*v1 + xi*t(l1,m1)*v2
+        sum(4) = sum(4) - ieo * (s(l1,m1)*v1 - xi*t(l1,m1)*v2)
+        sum(5) = sum(5) + r(l1,m1)*p(l1)
+        sum(6) = sum(6) + ieo * r(l1,m1)*p(l1)
 
       END DO
 
 ! Combine with numerical factors and 1/sin(lat) where necessary
 ! With the above sums, these commands represent H88 eqns 9-11:
 ! - velocity components at pixel latitude theta_j for harmonic order, m.
-      unorth(m1) = hfac*sum1*rst
-      usouth(m1) = hfac*sum2*rst
-      vnorth(m1) = hfac*sum3*rst
-      vsouth(m1) = hfac*sum4*rst
-      wnorth(m1) = hfac*sum5
-      wsouth(m1) = hfac*sum6
 
-      IF (m > 0) THEN
+      DO isum = 1, 6
 
-        unorth(m2) = hfac*CONJG(sum1)*rst
-        usouth(m2) = hfac*CONJG(sum2)*rst
-        vnorth(m2) = hfac*CONJG(sum3)*rst
-        vsouth(m2) = hfac*CONJG(sum4)*rst
-        wnorth(m2) = hfac*CONJG(sum5)
-        wsouth(m2) = hfac*CONJG(sum6)
+        IF (isum > 4) THEN
+          rstterm = 1
+        ELSE
+          rstterm = rst
+        END IF
 
-      END IF
+        vc(isum,m1) = hfac*sum(isum)*rstterm
+        IF (m > 0) vc(isum,m2) = hfac*CONJG(sum(isum))*rstterm
+
+      END DO
 
     END DO
 
@@ -134,29 +119,23 @@ SUBROUTINE calc_velocity_components(r, s, t, u, v, w)
 !***********************************************************************
 
 ! Initialize all N and S hemisphere complex velocity components to zero
-    unorth(lmax+1:nphi-lmax+1) = 0.
-    usouth(lmax+1:nphi-lmax+1) = 0.
-    vnorth(lmax+1:nphi-lmax+1) = 0.
-    vsouth(lmax+1:nphi-lmax+1) = 0.
-    wnorth(lmax+1:nphi-lmax+1) = 0.
-    wsouth(lmax+1:nphi-lmax+1) = 0.
+    DO isum = 1, 6
+      vc(isum,lmax+1:nphi-lmax+1) = 0.
+    END DO
 
 ! Take FFT along latitudinal strip to determine complex velocities
-    call four1_sp(unorth,nphi,-1)
-    call four1_sp(usouth,nphi,-1)
-    call four1_sp(vnorth,nphi,-1)
-    call four1_sp(vsouth,nphi,-1)
-    call four1_sp(wnorth,nphi,-1)
-    call four1_sp(wsouth,nphi,-1)
+    DO isum = 1, 6
+      call four1_sp(vc(isum,:),nphi,-1)
+    END DO
 
 ! Calculate velocity profile along latitudinal strip
 ! Take real part of complex velocities
-    u(1:nphi,jn) = REAL(unorth(1:nphi))
-    u(1:nphi,js) = REAL(usouth(1:nphi))
-    v(1:nphi,jn) = REAL(vnorth(1:nphi))
-    v(1:nphi,js) = REAL(vsouth(1:nphi))
-    w(1:nphi,jn) = REAL(wnorth(1:nphi))
-    w(1:nphi,js) = REAL(wsouth(1:nphi))
+    u(1:nphi,jn) = REAL(vc(1, 1:nphi))
+    u(1:nphi,js) = REAL(vc(2, 1:nphi))
+    v(1:nphi,jn) = REAL(vc(3, 1:nphi))
+    v(1:nphi,js) = REAL(vc(4, 1:nphi))
+    w(1:nphi,jn) = REAL(vc(5, 1:nphi))
+    w(1:nphi,js) = REAL(vc(6, 1:nphi))
 
   END DO
 
