60ec96b00b88462862d3e7f717420079fe2da0f7
SolarDub <81772405+SolarDub@users.noreply.github.com>
2023-08-10 14:15:12 -0400

    Made arg function

--- src/f90/calc_conv_spec.f90
+++ src/f90/calc_conv_spec.f90
@@ -5,24 +5,14 @@ SUBROUTINE calc_conv_spec(ihour, amp, r, s, t)
 
   IMPLICIT NONE
 
-  INTERFACE
-    SUBROUTINE getArg(ihour, m, factor2, factor4, arg)
-      USE mod_main, ONLY : SP
-      INTEGER, INTENT(IN) :: ihour, m
-      REAL(kind=SP), INTENT(IN) :: factor2, factor4
-      REAL(kind=SP) :: hours, phase
-      COMPLEX(kind=SP), INTENT(OUT)    :: arg
-    END SUBROUTINE getArg
-  END INTERFACE
-
   INTEGER, INTENT(IN) :: ihour
   INTEGER :: l, l1, m, m1, idummy
 
   REAL(kind=SP), DIMENSION(lmax,3), INTENT(IN) :: amp
-  REAL(kind=SP) :: hours, el, em, factor2, factor4, phase
+  REAL(kind=SP) :: hours, el, phase
 
 ! Exponential part of polar complex number
-  COMPLEX(kind=SP) :: arg
+  COMPLEX(kind=SP) :: getArg, arg
 
   COMPLEX(kind=SP), DIMENSION(nx,nx), INTENT(INOUT) :: r, s, t
 
@@ -50,13 +40,10 @@ SUBROUTINE calc_conv_spec(ihour, amp, r, s, t)
 
           arg = (0., 0.)
   	      m1 = m+1
-  	      em = float(m)/sqrt(l*(l+1.))
-  	      factor2 = (1.-em*em)
-  	      factor4 = (1.-em*em*em*em)
   !
   ! Add phase information for rotation (r & s use same arg value)
   !
-          CALL getArg(ihour, m, factor2, factor4, arg)
+          arg = getArg(ihour, l, m)
 
   	      r(l1,m1) = -1.*amp(l,1)*arg
   		    s(l1,m1) = amp(l,2)*arg
@@ -64,8 +51,7 @@ SUBROUTINE calc_conv_spec(ihour, amp, r, s, t)
   !
   ! Use random phases for toroidal component (t uses different arg value)
   !
-          CALL getArg(ihour, m, factor2, factor4, arg)
-
+          arg = getArg(ihour, l, m)
   		    t(l1,m1) = amp(l,3)*arg
 
   	      END DO
@@ -81,22 +67,25 @@ END SUBROUTINE calc_conv_spec
 !                                                      *
 !*******************************************************
 
-SUBROUTINE getArg(ihour, m, factor2, factor4, arg)
+COMPLEX FUNCTION getArg(ihour, l, m)
 
   USE mod_main, ONLY : SP
   USE mod_params, ONLY : pi, xi, omega0, omega2, omega4
 
   IMPLICIT NONE
 
-  INTEGER, INTENT(IN) :: ihour, m
-  REAL(kind=SP), INTENT(IN) :: factor2, factor4
-  REAL(kind=SP) :: hours, phase
-  COMPLEX(kind=SP), INTENT(OUT)    :: arg
+  INTEGER, INTENT(IN) :: ihour, l, m
+  REAL(kind=SP) :: hours, em, factor2, factor4, phase
+  COMPLEX(kind=SP) :: argmt
 
   hours = float(ihour)
 
+  em = float(m)/sqrt(l*(l+1.))
+  factor2 = (1.-em*em)
+  factor4 = (1.-em*em*em*em)
+
   phase = 2.*pi * rand()  ! random phase for spectral amps
   phase = phase + m*hours * (omega0 + omega2*factor2 + omega4*factor4)
-  arg = cos(phase) + xi*sin(phase)
+  argmt = cos(phase) + xi*sin(phase)
 
-END SUBROUTINE getArg
+END FUNCTION getArg
